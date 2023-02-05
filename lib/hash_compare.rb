28f489cb15effdd5d05df88a4666ec7a1a51d007
Zoltan Kiss <zoltan.a.kiss@gmail.com>
2023-02-05 18:37:47 +0100

    add `HashCompare.to_s` and support shallow diff as well

--- lib/hash_compare.rb
+++ lib/hash_compare.rb
@@ -4,7 +4,81 @@ require_relative "../lib/traversal"
 
 class HashCompare
   class << self
+    # This code is unfortunately not very pretty. It was
+    # intended as a interesting pontential real-world use case for my other
+    # code, given how similar the output is to the format that
+    # git uses. (e.g git rebase conflict). Could be very useful for large
+    # hashes.
+    #
+    # ex output:
+    #
+    # a =>
+    #   <<<<<< hash1
+    #   =======
+    #   a3
+    #   >>>>>> hash2
+    #   c =>
+    #     <<<<<< hash1
+    #     e
+    #     d1
+    #     =======
+    #     d
+    #     >>>>>> hash2
+    # b =>
+    #   <<<<<< hash1
+    #   c
+    #   =======
+    #   >>>>>> hash2
+    def to_s(obj1, obj2)
+      diff = diff(obj1, obj2, shallow: false)
+      already_executed = {}
+      lines = []
+
+      diff.to_a.sort { |e| e.length }.reverse.each do |e|
+        k = e.first
+        v = e.last
+
+        k.length.times do |k1|
+          prefix = k[0..k1]
+          next if already_executed[prefix]
+          lines << "#{'  '*k1}#{k[k1]} =>"
+          if prefix.join("") != k && !already_executed[prefix] && diff[prefix]
+            lines << "#{'  '*(k1+1)}<<<<<< hash1"
+            diff[prefix].select { |k2, v2| v2 == "-" }.keys.each do |hash1e|
+              lines << "#{'  '*(k1+1)}#{hash1e}"
+            end
+            lines << "#{'  '*(k1+1)}======="
+            diff[prefix].select { |k2, v2| v2 == "+" }.keys.each do |hash1e|
+              lines << "#{'  '*(k1+1)}#{hash1e}"
+            end
+            lines << "#{'  '*(k1+1)}>>>>>> hash2"
+          end
+          already_executed[prefix] = true
+        end
+      end
+
+      lines.join("\n")
+    end
+
+    def shallow_diff(obj1, obj2)
+      keys_added = obj2.keys - obj1.keys
+      keys_subtracted = obj1.keys - obj2.keys
+      keys_diff = keys_added.to_h { |e| [e, "+"] }.merge(
+        keys_subtracted.to_h { |e| [e, "-"] }
+      )
+
+      vals_added = obj2.values - obj1.values
+      vals_subtracted = obj1.values - obj2.values
+      vals_diff = vals_added.to_h { |e| [e, "+"] }.merge(
+        vals_subtracted.to_h { |e| [e, "-"] }
+      )
+
+      { keys_diff: keys_diff, vals_diff: vals_diff }
+    end
+
     def diff(obj1, obj2, shallow: false)
+      return shallow_diff(obj1, obj2) if shallow
+
       h = Traversal.traverse(obj1)
       h2 = Traversal.traverse(obj2)
       h3 = {}
--- test/hash_compare_test.rb
+++ test/hash_compare_test.rb
@@ -5,6 +5,55 @@ require "minitest/autorun"
 require_relative "../lib/hash_compare"
 
 class HashCompareTest < Minitest::Test
+  def test_to_s
+    hash1 = {
+      "a" => [
+        "a1",
+        "a2",
+        "a3",
+        {
+          "c" => ["d", true]
+        }
+      ]
+    }
+
+    hash2 = {
+      "a" => [
+        "a1",
+        "a2",
+        "a4",
+        {
+          "c" => ["e", "d1"]
+        }
+      ],
+      "b" => "c"
+    }
+
+    results = <<~STRING
+a =>
+  <<<<<< hash1
+  a3
+  =======
+  a4
+  >>>>>> hash2
+  c =>
+    <<<<<< hash1
+    d
+    true
+    =======
+    e
+    d1
+    >>>>>> hash2
+b =>
+  <<<<<< hash1
+  =======
+  c
+  >>>>>> hash2
+    STRING
+
+    assert_equal results.strip, HashCompare.to_s(hash1, hash2).strip
+  end
+
   def test_hash_compare_deep
     hash1 = {
       "a" => [
@@ -38,5 +87,23 @@ class HashCompareTest < Minitest::Test
   end
 
   def test_hash_compare_shallow
+    hash1 = {
+      "a" => "b",
+      "b" => "c1",
+      "c" => "c"
+    }
+
+    hash2 = {
+      "a" => "b",
+      "b" => "c",
+      "d" => "d"
+    }
+
+    diff = {
+      keys_diff: { "c" => "-", "d" => "+" },
+      vals_diff: { "c1" => "-", "d" => "+" }
+    }
+
+    assert_equal diff, HashCompare.diff(hash1, hash2, shallow: true)
   end
 end
